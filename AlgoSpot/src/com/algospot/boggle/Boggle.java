6099eed4858a95b6d0a1ebc61640e88d241e2b8b
imdasom <imdasom101@gmail.com>
2018-01-07 21:03:22 +0900

    algo spot 프로젝트 생성

---
+++ AlgoSpot/src/com/algospot/boggle/Boggle.java
@@ -0,0 +1,15 @@
+package com.algospot.boggle;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Boggle {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(new File("src\\com\\algospot\\boggle\\input.txt")));
+		
+	}
+
+}
